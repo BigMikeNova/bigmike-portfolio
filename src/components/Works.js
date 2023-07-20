c1e55677e7147e9485cb735b9bd30d0c2a3d4d71
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-07-19 21:44:44 -0400

    adding all my files because local dev babygit add -A git add -A !

---
+++ src/components/Works.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import beer from '../assets/beer-traveler.jpg';
+import food from '../assets/foodbg.png';
+import cheatSheet from '../assets/css-logo.png'
+
+const Works = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8 w-full flex justify-center items-center flex-col'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
+            Work
+          </p>
+          <p className='py-6 text-2xl'>Check out some of my most recent work</p>
+        </div>
+{/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${beer})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Beer Traveler
+              </span>
+              <p className='text-center'>An API Linking App for Finding Breweries in your city on a nice day</p>
+              <div className='pt-8 text-center'>
+                <a href='https://jkrieger6.github.io/beer-traveler/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/jkrieger6/beer-traveler'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${food})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                The Recipe Drawer
+              </span>
+              <p className='text-center'>A barebones social media site for recipe sharing</p>
+              <div className='pt-8 text-center'>
+                <a href='https://the-recipe-drawer.herokuapp.com/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/BigMikeNova/the-recipe-drawer'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${cheatSheet})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                CSS CheatSheet
+              </span>
+              <p className='text-center'>A cheatsheet for new coders to learn CSS</p>
+              <div className='pt-8 text-center'>
+                <a href='https://bigmikenova.github.io/CSS-Snippet-Cheatsheet/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/BigMikeNova/CSS-Snippet-Cheatsheet'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>        
+</div>
+  );
+};
+export default Works;
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const projects = [
+
+    {
+      title: 'The Recipe Drawer',
+      image: 'images/foodbg.png',
+      deployedLink: 'https://the-recipe-drawer.herokuapp.com/',
+      githubLink: 'https://github.com/BigMikeNova/the-recipe-drawer'
+    },
+    {
+      title: 'CSS CheatSheet',
+      image: 'images/HTML CSS.webp',
+      deployedLink: 'https://bigmikenova.github.io/CSS-Snippet-Cheatsheet/',
+      githubLink: 'https://github.com/BigMikeNova/CSS-Snippet-Cheatsheet'
+    }
+  ];
+
